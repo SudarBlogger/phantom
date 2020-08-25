ab0abda09a8f9024a8bc1e1fbd533bf5c91c48f0
Arlina Design <Arlina-Design@users.noreply.github.com>
2020-08-26 05:35:55 +0700

    Update and rename arlinadzgnin.js to arlinadzgninn.js

--- arlinadzgnin.js
+++
@@ -1,8 +0,0 @@
-//Spoiler
-$(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
-// Youtube Responsive
-setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<iframe class="video-youtube loader" src="'+$(this).data("src")+'" allowfullscreen="allowfullscreen" height="281" width="500"></iframe>')})},5e3);
-// Disqus One
-!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//"+disqus_shortname+".disqus.com/blogger_item.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
-// Code Pre
-$('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
